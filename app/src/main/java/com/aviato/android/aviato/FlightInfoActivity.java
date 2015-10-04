cd71493cddd828bfd32140bf3b294ea55895dcc3
MarcioPorto <mflporto@gmail.com>
2015-10-04 23:32:01 +0800

    Finished layout and setup for first two activities. Now working on transport modes.

--- app/src/main/java/com/aviato/android/aviato/FlightInfoActivity.java
+++ app/src/main/java/com/aviato/android/aviato/FlightInfoActivity.java
@@ -1,16 +1,52 @@
 package com.aviato.android.aviato;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class FlightInfoActivity extends AppCompatActivity {
+
+    public static final String TAG = FlightInfoActivity.class.getSimpleName();
+
+    private TextView mDepartureAirportCode;
+    private TextView mDepartureAirportCity;
+    private TextView mArrivalAirportCode;
+    private TextView mArrivalAirportCity;
+    private TextView mFlightNumberLabel;
+
+    private TextView mFlightStatusValue;
+    private TextView mFlightBoardingValue;
+    private TextView mFlightGateValue;
+
+    private String mFlightNumber;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_flight_info);
+
+        Intent intent = getIntent();
+        mFlightNumber = intent.getStringExtra(getString(R.string.flight_number_intent_extra));
+
+        if (mFlightNumber == null) {
+            //TODO: handle this later
+        }
+        Log.d(TAG, mFlightNumber);
+
+        mDepartureAirportCode = (TextView)findViewById(R.id.departure_airport_code);
+        mDepartureAirportCity = (TextView)findViewById(R.id.departure_airport_city);
+        mArrivalAirportCode = (TextView)findViewById(R.id.arrival_airport_code);
+        mArrivalAirportCity = (TextView)findViewById(R.id.arrival_airport_city);
+        mFlightNumberLabel = (TextView)findViewById(R.id.flight_number_label);
+
+        mFlightStatusValue = (TextView)findViewById(R.id.status_value);
+        mFlightBoardingValue = (TextView)findViewById(R.id.boarding_value);
+        mFlightGateValue = (TextView)findViewById(R.id.gate_value);
+
     }
 
     @Override
